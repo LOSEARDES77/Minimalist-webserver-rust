c5c60befb0b383b08b746bf83a2a41987c06de96
LOSEARDES77 <loseardes77@gmail.com>
2024-06-09 20:34:44 +0200

    improve multithread lib

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,24 +1,76 @@
-use std::net::{TcpListener, TcpStream};
+use multithreading::ThreadPool;
 use std::env::args;
 use std::io::{Read, Write};
-use http_server::ThreadPool;
+use std::net::{IpAddr, TcpListener, TcpStream};
+
+struct Address {
+    ip: IpAddr,
+    port: Port,
+}
+
+struct Port {
+    port: u16,
+}
+
+impl Port {
+    fn new(port: String) -> Option<Self> {
+        let port = match port.parse() {
+            Ok(port) => port,
+            Err(_) => {
+                println!("Invalid port number");
+                return None;
+            }
+        };
+        Some(Port { port })
+    }
+
+    fn as_str(&self) -> String {
+        format!("{}", self.port)
+    }
+}
+
+impl Address {
+    fn new(ip: String, port: String) -> Option<Self> {
+        let ip = match ip.parse() {
+            Ok(ip) => ip,
+            Err(_) => {
+                println!("Invalid IP address");
+                return None;
+            }
+        };
+
+        let port = match Port::new(port) {
+            Some(port) => port,
+            None => {
+                println!("Invalid port number");
+                return None;
+            }
+        };
+
+        Some(Address { ip, port })
+    }
+
+    fn as_str(&self) -> String {
+        format!("{}:{}", self.ip.to_string(), self.port.as_str())
+    }
+}
 
 fn main() {
-let address;
+    let address;
     // CLI
-    let mut workers = num_cpus::get() / 2;
+    let mut workers = 8;
     {
         let args = args().collect::<Vec<String>>();
         let mut port = String::new();
-        let mut ip= String::new();
+        let mut ip = String::new();
         for i in 0..args.len() {
             if args[i] == "-p" || args[i] == "--port" {
                 port = args[i + 1].clone();
             }
-            if args[i] == "-ip" || args[i] == "--address"{
+            if args[i] == "-ip" || args[i] == "--address" {
                 ip = args[i + 1].clone();
             }
-            if args[i] == "-j" || args[i] == "--workers"{
+            if args[i] == "-j" || args[i] == "--workers" {
                 workers = args[i + 1].parse::<usize>().unwrap();
             }
             if args[i] == "--help" || args[i] == "-h" {
@@ -49,46 +101,58 @@ let address;
         if ip.is_empty() {
             ip = "127.0.0.1".to_string();
         }
-        address = format!("{}:{}", ip, port);
+        address = Address::new(ip, port);
     }
 
+    let address = match address {
+        Some(addr) => addr,
+        None => {
+            println!("Invalid address");
+            return;
+        }
+    };
+
     let listener = TcpListener::bind(address.as_str()).unwrap();
-    println!("Listening on {}", address);
+    println!("Listening on {}", address.as_str());
     println!("Using {} workers", workers);
     let pool = ThreadPool::new(workers);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         let address = &stream.peer_addr().unwrap();
-        println!("Received request from {} port {}", address.ip(), address.port());
+        println!(
+            "Received request from {} port {}",
+            address.ip(),
+            address.port()
+        );
 
         pool.execute(|| {
             handle_connection(stream);
         });
-
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    if !buffer.starts_with(b"GET") { return; }
-    let file = buffer.split(|&x| x == b' ').collect::<Vec<&[u8]>>()[1];
-    let file = String::from_utf8_lossy(file).to_string();
+    if !buffer.starts_with(b"GET") {
+        return;
+    }
+    let file = String::from_utf8_lossy(buffer.split(|&x| x == b' ').collect::<Vec<&[u8]>>()[1])
+        .to_string();
     let file = parse_file(file);
     let file = format!("./{}", &file);
     println!("Serving file: {}", file);
-    let file_contents = String::from_utf8_lossy(&std::fs::read(file).unwrap_or_default()).to_string();
-
+    let file_contents =
+        String::from_utf8_lossy(&std::fs::read(file).unwrap_or_default()).to_string();
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let response;
     if file_contents.is_empty() {
-        let response = get_response(404, "Not Found", "404 Not Found".to_string());
-        stream.write(response.as_ref()).unwrap();
-        stream.flush().unwrap();
-        return;
+        response = get_response(404, "Not Found", "404 Not Found".to_string());
+    } else {
+        response = get_response(200, "OK", file_contents);
     }
-    let response = get_response(200, "OK", file_contents);
     stream.write_all(response.as_ref()).unwrap();
 }
 
@@ -107,5 +171,11 @@ fn parse_file(file: String) -> String {
 }
 
 fn get_response(code: u16, status_line_message: &str, message: String) -> String {
-    format!("HTTP/1.1 {} {}\r\nContext-Length: {}\r\n\r\n{}", code, status_line_message, message.len(), &message)
+    format!(
+        "HTTP/1.1 {} {}\r\nContext-Length: {}\r\n\r\n{}",
+        code,
+        status_line_message,
+        message.len(),
+        &message
+    )
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
-use std::sync::{Arc, mpsc, Mutex};
-use std::thread;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread::{self, JoinHandle};
 
 pub struct ThreadPool {
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Message>>,
+    workers: Option<Vec<Worker>>,
+}
+
+enum Message {
+    NewJob(Job),
+    Terminate,
 }
 
 impl ThreadPool {
@@ -11,34 +17,112 @@ impl ThreadPool {
 
         let (sender, receiver) = mpsc::channel();
         let receiver = Arc::new(Mutex::new(receiver));
-        // let mut workers = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
         for id in 0..size {
-            Worker::new(id, Arc::clone(&receiver));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { sender }
+        ThreadPool {
+            sender: Some(sender),
+            workers: Some(workers),
+        }
     }
-    pub fn execute<F>(&self, f: F)
+    pub fn execute<F>(&self, f: F) -> Result<(), String>
     where
         F: FnOnce() + Send + 'static,
     {
+        if self.sender.is_none() {
+            return Err("ThreadPool is not initialized No Sender".to_string());
+        }
+
+        if self.workers.is_none() {
+            return Err("ThreadPool is not initialized (No Workers)".to_string());
+        }
+
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+
+        let res = match self.sender.as_ref().unwrap().send(Message::NewJob(job)) {
+            Ok(_) => Ok(()),
+            Err(e) => Err("Error sending job to worker \"".to_string() + &e.to_string() + "\"\n"),
+        };
+
+        res
     }
 
+    pub fn shutdown(&mut self) -> Result<(), String> {
+        if self.workers.is_none() {
+            return Err("ThreadPool is not initialized (No Workers)".to_string());
+        }
+
+        for _ in 0..self.workers.as_ref().unwrap().len() {
+            self.sender
+                .as_ref()
+                .unwrap()
+                .send(Message::Terminate)
+                .unwrap();
+        }
+
+        for worker in self.workers.as_mut().unwrap() {
+            worker.join();
+        }
+
+        self.workers = None;
+        self.sender = None;
+
+        Ok(())
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        if self.workers.is_some() {
+            for _ in 0..self.workers.as_ref().unwrap().len() {
+                self.sender
+                    .as_ref()
+                    .unwrap()
+                    .send(Message::Terminate)
+                    .unwrap();
+            }
+
+            for worker in self.workers.as_mut().unwrap() {
+                worker.join();
+            }
+
+            self.workers = None;
+            self.sender = None;
+        }
+    }
 }
 
 struct Worker {
+    thread: Option<JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        /* let thread = */ thread::spawn(move || loop {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
             let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job; executing.", id);
-            job();
+            match job {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            }
             println!("Worker {} finished the job.", id)
         });
-        Worker { }
+
+        Worker {
+            thread: Some(thread),
+        }
+    }
+
+    fn join(&mut self) {
+        if let Some(thread) = self.thread.take() {
+            thread.join().unwrap();
+        }
     }
 }
 
