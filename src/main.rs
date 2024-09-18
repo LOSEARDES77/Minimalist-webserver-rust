770894069e1e753cefd8548da641cb02a376f7bf
LOSEARDES77 <84414230+LOSEARDES77@users.noreply.github.com>
2024-09-18 12:31:07 +0200

    update and use clap

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,52 @@
-use std::net::{TcpListener, TcpStream};
+use clap::Parser;
+use multithreading::ThreadPool;
 use std::env::args;
 use std::io::{Read, Write};
-use multithreading::ThreadPool;
+use std::net::{TcpListener, TcpStream};
 
-fn main() {
-let address;
-    // CLI
-    let mut workers = num_cpus::get() / 2;
-    {
-        let args = args().collect::<Vec<String>>();
-        let mut port = String::new();
-        let mut ip= String::new();
-        for i in 0..args.len() {
-            if args[i] == "-p" || args[i] == "--port" {
-                port = args[i + 1].clone();
-            }
-            if args[i] == "-ip" || args[i] == "--address"{
-                ip = args[i + 1].clone();
-            }
-            if args[i] == "-j" || args[i] == "--workers"{
-                workers = args[i + 1].parse::<usize>().unwrap();
-            }
-            if args[i] == "--help" || args[i] == "-h" {
-                #[cfg(windows)]
-                println!("Usage: {} [OPTIONS]\n\
-                          \tIf no options it will use 127.0.0.1:80 and half of you threads as default\n\
-                          \t-p <port>, --port <port>                -  Port to listen on\n\
-                          \t-ip <ip>, --address <ip>                -  IP to listen on\n\
-                          \t-j <workers>, --workers <workers>       -  Number of workers to use\n\
-                          \t--help, -h                              -  Print this help message",
-                         args[0].split("\\").collect::<Vec<&str>>().last().unwrap()
-                );
-                #[cfg(unix)]
-                println!("Usage: {} [OPTIONS]\n\
-                          \tIf no options it will use 127.0.0.1:80 and half of you threads as default\n\
-                          \t-p <port>, --port <port>                -  Port to listen on\n\
-                          \t-ip <ip>, --address <ip>                -  IP to listen on\n\
-                          \t-j <workers>, --workers <workers>       -  Number of workers to use\n\
-                          \t--help, -h                              -  Print this help message",
-                         args[0].split("/").collect::<Vec<&str>>().last().unwrap()
-                );
-                return;
-            }
-        }
-        if port.is_empty() {
-            port = "80".to_string();
-        }
-        if ip.is_empty() {
-            ip = "127.0.0.1".to_string();
-        }
-        address = format!("{}:{}", ip, port);
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long, default_value_t = get_ip_address())]
+    address: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16,
+
+    #[arg(short, long, default_value_t = num_cpus::get() / 2)]
+    workers: usize,
+}
+
+fn get_ip_address() -> String {
+    if cfg!(debug_assertions) {
+        "127.0.0.1".parse().unwrap()
+    } else {
+        "0.0.0.0".parse().unwrap()
     }
+}
+fn main() {
+    let parsed_args = Args::parse();
+
+    let address = format!("{}:{}", parsed_args.address, parsed_args.port);
+
+    let workers = parsed_args.workers;
 
     let listener = match TcpListener::bind(address.as_str()) {
         Ok(listener) => listener,
-        Err(_) => {
-            println!("Error: Could not open port {} due to insufficient permission", address);
-            println!("tip: try running \"sudo setcap cap_net_bind_service=+ep {}\" to add permission or run it as sudo", args().collect::<Vec<String>>()[0]);
-            return;
+        Err(e) => {
+            if e.kind() == std::io::ErrorKind::PermissionDenied {
+                println!(
+                    "{}\nError: Could not open port {} due to insufficient permission",
+                    parsed_args.port, e
+                );
+                #[cfg(target_os = "linux")]
+                println!("tip: try running \"sudo setcap cap_net_bind_service=+ep {}\" to add permission or run it as sudo", args().collect::<Vec<String>>()[0]);
+            } else {
+                println!("Error: {}", e);
+            }
+            panic!();
         }
     };
 
@@ -68,31 +57,43 @@ let address;
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         let address = &stream.peer_addr().unwrap();
-        println!("Received request from {} port {}", address.ip(), address.port());
+        println!(
+            "Received request from {} port {}",
+            address.ip(),
+            address.port()
+        );
 
         pool.execute(|| {
             handle_connection(stream);
         });
-
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    if !buffer.starts_with(b"GET") { return; }
-    let file = buffer.split(|&x| x == b' ').collect::<Vec<&[u8]>>()[1];
-    let file = String::from_utf8_lossy(file).to_string();
+    let bytes_read = stream.read(&mut buffer).unwrap();
+    let buffer = String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
+
+    if !buffer.starts_with("GET") {
+        return;
+    }
+    let file = buffer.split_whitespace().nth(1).unwrap_or_default();
     let file = parse_file(file);
+    if file == "error_path_in_reverse" {
+        let response = get_response(403, "Forbidden", "403 Forbidden".to_string());
+        stream.write_all(response.as_ref()).unwrap();
+        stream.flush().unwrap();
+        return;
+    }
     let file = format!("./{}", &file);
     println!("Serving file: {}", file);
-    let file_contents = String::from_utf8_lossy(&std::fs::read(file).unwrap_or_default()).to_string();
+    let file_contents =
+        String::from_utf8_lossy(&std::fs::read(file).unwrap_or_default()).to_string();
 
-
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    println!("Request: {}", &buffer);
     if file_contents.is_empty() {
         let response = get_response(404, "Not Found", "404 Not Found".to_string());
-        stream.write(response.as_ref()).unwrap();
+        stream.write_all(response.as_ref()).unwrap();
         stream.flush().unwrap();
         return;
     }
@@ -100,10 +101,10 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write_all(response.as_ref()).unwrap();
 }
 
-fn parse_file(file: String) -> String {
-    let mut file = file;
+fn parse_file(file: &str) -> String {
+    let mut file = file.to_string();
     if file.contains("../") {
-        return "".to_string();
+        return "error_path_in_reverse".to_string();
     }
     if file.ends_with("/") {
         file = format!("{}index.html", file);
@@ -111,9 +112,15 @@ fn parse_file(file: String) -> String {
     if file.starts_with("/") {
         file = file[1..].parse().unwrap();
     }
-    return file;
+    file
 }
 
 fn get_response(code: u16, status_line_message: &str, message: String) -> String {
-    format!("HTTP/1.1 {} {}\r\nContext-Length: {}\r\n\r\n{}", code, status_line_message, message.len(), &message)
+    format!(
+        "HTTP/1.1 {} {}\r\nContext-Length: {}\r\n\r\n{}",
+        code,
+        status_line_message,
+        message.len(),
+        &message
+    )
 }
